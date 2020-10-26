bbc25c4ecb8f2db5500430bc69074c2be39104ba
armengev <64475505+armengev@users.noreply.github.com>
2020-10-27 00:58:58 +0400

    Create dynamodb.tf

---
+++ dynamodb.tf
@@ -0,0 +1,9 @@
+resource "aws_dynamodb_table" "terraform_state_lock" {
+    hash_key = "LockID"
+    read_capacity = 10
+    write_capacity = 10
+    name = "notejam-state-lock"
+    attribute {
+        name = "LockID"
+        type = "S"
+    }
